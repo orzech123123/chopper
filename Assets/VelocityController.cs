1ad17aaecd748fdfc71ecf9a8f93615905ed0043
orzech <michalorzechowski123@gmai.com>
2020-10-16 21:55:40 +0200

    init

---
+++ Assets/VelocityController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VelocityController : MonoBehaviour
+{
+    [SerializeField]
+    private int _force = 250;
+
+    [SerializeField]
+    private Joystick _joystick;
+
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Math.Abs(_joystick.Vertical) > 0.01f)
+        {
+            _rigidbody.AddForce(transform.up * _force * _joystick.Vertical, ForceMode.Acceleration);
+        }
+        if (Math.Abs(_joystick.Horizontal) > 0.01f)
+        {
+            _rigidbody.AddTorque(transform.up * _joystick.Horizontal, ForceMode.Acceleration);
+        }
+
+        if (Input.GetMouseButton(0) && Input.mousePosition.x > Screen.width / 2f)
+        {
+            _rigidbody.AddForce(transform.forward * 5f, ForceMode.Acceleration);
+        }
+
+        // Debug.Log(_joystick.Horizontal + " " + _joystick.Vertical);
+    }
+}   
