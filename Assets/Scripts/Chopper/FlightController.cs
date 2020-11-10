d2f6606b7e01d02a15f3a46dd8ac91aba91aa29c
orzech <orzech@DESKTOP-BE8VQUO>
2020-11-10 02:07:07 +0100

    keep altitude tests in progress

--- Assets/Scripts/Chopper/FlightController.cs
+++ Assets/Scripts/Chopper/FlightController.cs
@@ -6,13 +6,12 @@ namespace Assets.Scripts.Chopper
     [RequireComponent(typeof(Rigidbody))]
     public class FlightController : MonoBehaviour
     {
-        [SerializeField]
-        private int _force = 250;
-        [SerializeField]
-        private Joystick _leftJoystick;
-        [SerializeField]
-        private Joystick _rightJoystick;
+        public float MaxVelocity = 10f;
+        public float Force = 25f;
+        public Joystick LeftJoystick;
+        public Joystick RightJoystick;
         private Rigidbody _rigidBody;
+        private float? _keepAltitude;
 
         void Start()
         {
@@ -22,22 +21,39 @@ namespace Assets.Scripts.Chopper
 
         void FixedUpdate()
         {
-            if (Math.Abs(_leftJoystick.Vertical) > 0.01f)
+            var forwardVelocity = Vector3.Dot(_rigidBody.velocity, transform.forward);
+            var altitudeChangedByInput = false;
+
+            if (Math.Abs(LeftJoystick.Vertical) > 0.01f)
             {
-                _rigidBody.AddForce(transform.up * _force * _leftJoystick.Vertical, ForceMode.Acceleration);
-            }
-            if (Math.Abs(_leftJoystick.Horizontal) > 0.01f)
-            {
-                _rigidBody.AddForce(transform.right * 7f * _leftJoystick.Horizontal, ForceMode.Acceleration);
+                _rigidBody.AddForce(transform.up * Force * LeftJoystick.Vertical, ForceMode.Acceleration);
+                _keepAltitude = null;
+                altitudeChangedByInput = true;
             }
 
-            if (Math.Abs(_rightJoystick.Vertical) > 0.01f)
+            if (Math.Abs(LeftJoystick.Horizontal) > 0.01f)
             {
-                _rigidBody.AddForce(transform.forward * 8f * _rightJoystick.Vertical, ForceMode.Acceleration);
+                _rigidBody.AddForce(transform.right * 7f * LeftJoystick.Horizontal, ForceMode.Acceleration);
             }
-            if (Math.Abs(_rightJoystick.Horizontal) > 0.01f)
+
+            if (Math.Abs(RightJoystick.Vertical) > 0.01f && Math.Abs(forwardVelocity) < MaxVelocity)
             {
-                _rigidBody.AddTorque(transform.up * 0.2f * _rightJoystick.Horizontal, ForceMode.Acceleration);
+                _rigidBody.AddForce(transform.forward * 8f * RightJoystick.Vertical, ForceMode.Acceleration);
+            }
+            if (Math.Abs(RightJoystick.Horizontal) > 0.01f)
+            {
+                _rigidBody.AddTorque(transform.up * 0.2f * RightJoystick.Horizontal, ForceMode.Acceleration);
+            }
+
+            if (!altitudeChangedByInput)
+            {
+                if (_keepAltitude == null)
+                {
+                    _keepAltitude = transform.position.y;
+                }
+
+                var goHigher = transform.position.y <= _keepAltitude ? 1 : -1;
+                _rigidBody.AddForce(transform.up * Force * goHigher, ForceMode.Acceleration);
             }
         }
     }
