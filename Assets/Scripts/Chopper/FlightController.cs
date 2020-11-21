2a451c2afd794388eb28d322abf0ec82a599e72c
orzech <orzech@DESKTOP-BE8VQUO>
2020-11-21 21:16:08 +0100

    left right slow down

--- Assets/Scripts/Chopper/FlightController.cs
+++ Assets/Scripts/Chopper/FlightController.cs
@@ -18,8 +18,14 @@ namespace Assets.Scripts.Chopper
         public float VerticalSlowDownTotalTime = 5f;
         private float _verticalSlowDownDiffTime;
 
+        public float ForwardSlowDownTotalTime = 5f;
+        private float _forwardSlowDownDiffTime;
+
         public float RotationSlowDownTotalTime = 5f;
         private float _rotationSlowDownDiffTime;
+
+        public float LeftRightSlowDownTotalTime = 5f;
+        private float _leftRightSlowDownDiffTime;
 
         void Start()
         {
@@ -31,7 +37,7 @@ namespace Assets.Scripts.Chopper
         {
             MoveVertically();
 
-            MoveLeftAndRight();
+            MoveLeftRight();
 
             MoveForward();
 
@@ -70,15 +76,35 @@ namespace Assets.Scripts.Chopper
             if (Math.Abs(RightJoystick.Vertical) > 0.01f && Math.Abs(forwardVelocity) < MaxVelocity)
             {
                 _rigidBody.AddForce(transform.forward * 8f * RightJoystick.Vertical, ForceMode.Acceleration);
+                _forwardSlowDownDiffTime = 0f;
+            }
+            else
+            {
+                SlowDownForward();
             }
         }
 
-        private void MoveLeftAndRight()
+        private void MoveLeftRight()
         {
             if (Math.Abs(LeftJoystick.Horizontal) > 0.01f)
             {
                 _rigidBody.AddForce(transform.right * 7f * LeftJoystick.Horizontal, ForceMode.Acceleration);
+                _leftRightSlowDownDiffTime = 0f;
             }
+            else
+            {
+                SlowDownLeftRight();
+            }
+        }
+
+        private void SlowDownLeftRight()
+        {
+            _leftRightSlowDownDiffTime += Time.deltaTime / LeftRightSlowDownTotalTime;
+            var factor = Mathf.Lerp(0, 1, _leftRightSlowDownDiffTime);
+
+            var rightVelocity = Vector3.Dot(_rigidBody.velocity, transform.right);
+            var counterRightVelocity = -rightVelocity * factor;
+            _rigidBody.AddForce(transform.right * counterRightVelocity, ForceMode.VelocityChange);
         }
 
         private void SlowDownVertically()
@@ -99,6 +125,16 @@ namespace Assets.Scripts.Chopper
             var v = _rigidBody.angularVelocity;
             var yCounterVelocity = -v.y * factor;
             _rigidBody.AddTorque(new Vector3(0, yCounterVelocity, 0), ForceMode.VelocityChange);
+        }
+
+        private void SlowDownForward()
+        {
+            _forwardSlowDownDiffTime += Time.deltaTime / ForwardSlowDownTotalTime;
+            var factor = Mathf.Lerp(0, 1, _forwardSlowDownDiffTime);
+
+            var forwardVelocity = Vector3.Dot(_rigidBody.velocity, transform.forward);
+            var counterForwardVelocity = -forwardVelocity * factor;
+            _rigidBody.AddForce(transform.forward * counterForwardVelocity, ForceMode.VelocityChange);
         }
     }
 }   
