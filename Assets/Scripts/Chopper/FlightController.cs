db3b503418368c99a0d84ec9846f5571858f176d
orzech <orzech@DESKTOP-BE8VQUO>
2020-11-21 20:11:27 +0100

    slowing down chopper

--- Assets/Scripts/Chopper/FlightController.cs
+++ Assets/Scripts/Chopper/FlightController.cs
@@ -18,6 +18,9 @@ namespace Assets.Scripts.Chopper
         public float VerticalSlowDownTotalTime = 5f;
         private float _verticalSlowDownDiffTime;
 
+        public float RotationSlowDownTotalTime = 5f;
+        private float _rotationSlowDownDiffTime;
+
         void Start()
         {
             _rigidBody = GetComponent<Rigidbody>();
@@ -26,34 +29,55 @@ namespace Assets.Scripts.Chopper
 
         void FixedUpdate()
         {
-            var forwardVelocity = Vector3.Dot(_rigidBody.velocity, transform.forward);
-            var wasHeightChanged = false;
+            MoveVertically();
 
+            MoveLeftAndRight();
+
+            MoveForward();
+
+            RotateOnYAxis();
+        }
+
+        private void RotateOnYAxis()
+        {
+            if (Math.Abs(RightJoystick.Horizontal) > 0.01f)
+            {
+                _rigidBody.AddTorque(transform.up * 0.2f * RightJoystick.Horizontal, ForceMode.Acceleration);
+                _rotationSlowDownDiffTime = 0;
+            }
+            else
+            {
+                SlowDownRotation();
+            }
+        }
+
+        private void MoveVertically()
+        {
             if (Math.Abs(LeftJoystick.Vertical) > 0.01f)
             {
                 _rigidBody.AddForce(transform.up * Force * LeftJoystick.Vertical, ForceMode.Acceleration);
-
-                wasHeightChanged = true;
                 _verticalSlowDownDiffTime = 0;
             }
-
-            if (Math.Abs(LeftJoystick.Horizontal) > 0.01f)
+            else
             {
-                _rigidBody.AddForce(transform.right * 7f * LeftJoystick.Horizontal, ForceMode.Acceleration);
+                SlowDownVertically();
             }
+        }
 
+        private void MoveForward()
+        {
+            var forwardVelocity = Vector3.Dot(_rigidBody.velocity, transform.forward);
             if (Math.Abs(RightJoystick.Vertical) > 0.01f && Math.Abs(forwardVelocity) < MaxVelocity)
             {
                 _rigidBody.AddForce(transform.forward * 8f * RightJoystick.Vertical, ForceMode.Acceleration);
             }
-            if (Math.Abs(RightJoystick.Horizontal) > 0.01f)
-            {
-                _rigidBody.AddTorque(transform.up * 0.2f * RightJoystick.Horizontal, ForceMode.Acceleration);
-            }
+        }
 
-            if (!wasHeightChanged)
+        private void MoveLeftAndRight()
+        {
+            if (Math.Abs(LeftJoystick.Horizontal) > 0.01f)
             {
-                SlowDownVertically();
+                _rigidBody.AddForce(transform.right * 7f * LeftJoystick.Horizontal, ForceMode.Acceleration);
             }
         }
 
@@ -65,6 +89,16 @@ namespace Assets.Scripts.Chopper
             var v = _rigidBody.velocity;
             var yCounterVelocity = -v.y * factor;
             _rigidBody.AddForce(new Vector3(0, yCounterVelocity, 0), ForceMode.VelocityChange);
+        }
+
+        private void SlowDownRotation()
+        {
+            _rotationSlowDownDiffTime += Time.deltaTime / RotationSlowDownTotalTime;
+            var factor = Mathf.Lerp(0, 1, _rotationSlowDownDiffTime);
+
+            var v = _rigidBody.angularVelocity;
+            var yCounterVelocity = -v.y * factor;
+            _rigidBody.AddTorque(new Vector3(0, yCounterVelocity, 0), ForceMode.VelocityChange);
         }
     }
 }   
