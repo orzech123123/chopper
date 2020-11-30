7df3b5493ad55ac1a83bbccdd0b7d00a3839a51d
orzech <orzech@DESKTOP-BE8VQUO>
2020-11-30 22:53:42 +0100

    changes camera android fix

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -28,7 +28,14 @@ namespace Assets.Scripts.Chopper
 
         private void AdjustCamera()
         {
-            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+            var pointerX = Input.GetAxis("Mouse X");
+            //android:
+            if(Input.touchCount > 0)
+            {
+                pointerX = Input.touches[0].deltaPosition.x / Screen.width * 100;
+            }
+
+            offset = Quaternion.AngleAxis(pointerX * turnSpeed, Vector3.up) * offset;
             transform.position = player.position + offset;
             transform.LookAt(player.position);
             transform.Rotate(dowwRotationAngle, 0f, 0);
