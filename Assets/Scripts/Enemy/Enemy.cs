acdd56de02c885eb9b9029a31c5eec114169d38a
orzech <orzech@DESKTOP-BE8VQUO>
2020-12-24 01:47:10 +0100

    navmeshe enemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Chopper;
+using UnityEngine;
+using UnityEngine.AI;
 using Zenject;
 
 namespace Assets.Scripts.Enemy
@@ -9,12 +11,27 @@ namespace Assets.Scripts.Enemy
     }
 
     [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(NavMeshAgent))]
     public class Enemy : MonoBehaviour
     {
+        private NavMeshAgent _agent; 
+        private ChopperPlayer _player; 
+
         [Inject]
-        public void Construct(EnemyParams @params)
+        public void Construct(EnemyParams @params, ChopperPlayer player)
         {
             transform.position = @params.Position;
+            _player = player;
+        }
+
+        private void Start()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            _agent.SetDestination(new Vector3(_player.Chopper.position.x, 0, _player.Chopper.position.z));
         }
     }
 }
