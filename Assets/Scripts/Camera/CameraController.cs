c0d434ece74ecbfb102bdea159500b92d4104c92
orzech <orzech@DESKTOP-BE8VQUO>
2021-01-25 18:42:26 +0100

    zabawa kamera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,49 +1,34 @@
-﻿using UnityEngine;
+﻿using Cinemachine;
+using UnityEngine;
 
 namespace Assets.Scripts.Camera
 {
     public class CameraController : MonoBehaviour
     {
-        public Vector3 offset;
-        public float downRotationAngle = 15f;
+        public string HorizontalInput = "Mouse X";
+        public string VerticalInput = "Mouse Y";
+        public CinemachineFreeLook _vcam;
 
-
-        public float turnSpeed = 4.0f;
-        public Transform player;
-
-        private void Start()
+        void Awake()
         {
-            AdjustCamera(true);
+            _vcam = GetComponent<CinemachineFreeLook>();
+            _vcam.m_YAxis.m_InputAxisName = "";
+            _vcam.m_XAxis.m_InputAxisName = "";
         }
 
         void LateUpdate()
         {
-            if(UnityEngine.Input.GetMouseButton(0))
+            if (UnityEngine.Input.GetMouseButton(0))
             {
-                AdjustCamera();
+                _vcam.m_YAxis.m_InputAxisName = "Mouse Y";
+                _vcam.m_XAxis.m_InputAxisName = "Mouse X";
+            }
+            else
+            {
+                _vcam.m_YAxis.m_InputAxisName = "";
+                _vcam.m_XAxis.m_InputAxisName = "";
             }
         }
 
-        private void AdjustCamera(bool force = false)
-        {
-            var pointerX = UnityEngine.Input.GetAxis("Mouse X");
-            var position = UnityEngine.Input.mousePosition.x;
-            //android:
-            if (UnityEngine.Input.touchCount > 0)
-            {
-                pointerX = UnityEngine.Input.touches[0].deltaPosition.x / Screen.width * 100;
-                position = UnityEngine.Input.touches[0].rawPosition.x;
-            }
-
-            if((position < Screen.width * 0.3f || position > Screen.width * 2/3) && !force)
-            {
-                return;
-            }
-
-            offset = Quaternion.AngleAxis(pointerX * turnSpeed, Vector3.up) * offset;
-            transform.position = player.position + offset;
-            transform.LookAt(player.position);
-            transform.Rotate(downRotationAngle, 0f, 0);
-        }
     }
 }   
