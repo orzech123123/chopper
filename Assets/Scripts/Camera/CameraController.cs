c0e3bcc350a10ad21c8709ac5d85aa6076cf846c
orzech <orzech@DESKTOP-BE8VQUO>
2021-01-31 16:52:07 +0100

    camera follow fix

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,5 +1,6 @@
 ﻿using Cinemachine;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Assets.Scripts.Camera
 {
@@ -11,6 +12,8 @@ namespace Assets.Scripts.Camera
 
         void Awake()
         {
+            Screen.SetResolution(1560, 720, true);
+
             _vcam = GetComponent<CinemachineFreeLook>();
             _vcam.m_YAxis.m_InputAxisName = "";
             _vcam.m_XAxis.m_InputAxisName = "";
@@ -18,16 +21,21 @@ namespace Assets.Scripts.Camera
 
         void LateUpdate()
         {
-            if (UnityEngine.Input.GetMouseButton(0))
-            {
-                _vcam.m_YAxis.m_InputAxisName = "Mouse Y";
-                _vcam.m_XAxis.m_InputAxisName = "Mouse X";
-            }
-            else
-            {
-                _vcam.m_YAxis.m_InputAxisName = "";
-                _vcam.m_XAxis.m_InputAxisName = "";
-            }
+            _vcam.m_RecenterToTargetHeading.m_enabled = true;
+
+            _vcam.m_XAxis.Value = 0;
+            _vcam.m_YAxis.Value = 0.5f;
+            //Debug.Log(EventSystem.current.IsPointerOverGameObject());
+            //if (UnityEngine.Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+            //{
+            //    _vcam.m_YAxis.m_InputAxisName = "Mouse Y";
+            //    _vcam.m_XAxis.m_InputAxisName = "Mouse X";
+            //}
+            //else
+            //{
+            //    _vcam.m_YAxis.m_InputAxisName = "";
+            //    _vcam.m_XAxis.m_InputAxisName = "";
+            //}
         }
 
     }
